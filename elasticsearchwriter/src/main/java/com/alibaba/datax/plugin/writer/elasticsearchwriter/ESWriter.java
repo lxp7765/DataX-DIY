4ca871c96e9e60c95775417c2dea753b19905ed2
lixinpeng <lixinpeng@bailian.ai>
2022-05-24 21:19:31 +0800

    兼容时间类型

--- elasticsearchwriter/src/main/java/com/alibaba/datax/plugin/writer/elasticsearchwriter/ESWriter.java
+++ elasticsearchwriter/src/main/java/com/alibaba/datax/plugin/writer/elasticsearchwriter/ESWriter.java
@@ -371,14 +371,16 @@ public class ESWriter extends Writer {
                                 break;
                             case DATE:
                                 try {
-                                    String dateStr = getDateStr(columnList.get(i), column);
-                                    data.put(columnName, dateStr);
+                                    if (column.getByteSize() != 0) {
+                                        String dateStr = getDateStr(columnList.get(i), column);
+                                        data.put(columnName, dateStr);
+                                    }
                                 } catch (Exception e) {
                                     getTaskPluginCollector().collectDirtyRecord(record, String.format("时间类型解析失败 [%s:%s] exception: %s", columnName, column.toString(), e.toString()));
                                 }
                                 break;
                             case KEYWORD:
-                                if (columnList.get(i).getEmptyToNull() !=null && columnList.get(i).getEmptyToNull() && StringUtils.isEmpty(column.asString())) {
+                                if (columnList.get(i).getEmptyToNull() != null && columnList.get(i).getEmptyToNull() && StringUtils.isEmpty(column.asString())) {
                                     data.put(columnName, null);
                                     break;
                                 }
